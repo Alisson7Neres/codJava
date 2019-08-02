16ada47be2f9e8d5740e7aa9084d7e6932e94987
Alisson7Neres <53406391+Alisson7Neres@users.noreply.github.com>
2019-08-02 11:23:09 -0300

    Add files via upload

---
+++ Funcao01.java
@@ -0,0 +1,13 @@
+
+package funcao01;
+
+public class Funcao01 {
+    static void soma(int a, int b){ //uma função para ser exibida no main tem que ser static
+        int s = a + b;
+        System.out.println(s);
+    }
+    public static void main(String[] args) {
+        soma(57,83); 
+    }
+    
+}
---
+++ Funcao02.java
@@ -0,0 +1,12 @@
+package funcao02;
+public class Funcao02 {
+    static int mult(int n1, int n2){
+    int m = n1 * n2;
+    return m;
+}
+    public static void main(String[] args) {
+        int mostrar = mult(7,7);
+        System.out.println(mostrar);
+    }
+    
+}
